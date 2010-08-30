12f0324eb04330dbf204a5deb48e9ed40d4e1d1b
Sebastian Obermüller <sebastian@sharkoon.(none)>
2010-08-30 23:30:44 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3SubdomainDevise::Application.config.secret_token = 'ca4d3d7926f71dbfcbbf470d4c01b40fcf37fed72f1277048efe77418444fe8d10dcbbfeaea5f64660362198eaa7fcb081edfc4f65a90c76b2c2496bad010e26'
